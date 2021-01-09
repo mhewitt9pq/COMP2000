36c325b92af8570a3796d2a9489c8b7a33ae7e2e
mhewitt9pq <73035012+mhewitt9pq@users.noreply.github.com>
2021-01-09 15:50:57 +0000

    Creating kiosk interface

--- src/com/view/IKiosk.java
+++ src/com/view/IKiosk.java
@@ -1,5 +1,12 @@
 package com.view;
 
+import javax.swing.*;
+
 public class IKiosk {
 
+    private JPanel KioskPanel;
+    private JTextField textField1;
+    private JButton scanButton;
+    private JButton button1;
+    private JButton button2;
 }
