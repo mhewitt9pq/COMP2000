0cd0e0e12d38c70d66dce4dc864c743780e51d62
mhewitt9pq <73035012+mhewitt9pq@users.noreply.github.com>
2021-01-11 14:54:12 +0000

    Creating item object

--- src/com/model/Item.java
+++ src/com/model/Item.java
@@ -4,13 +4,65 @@ public class Item {
 
     private String name;
 
-    private double price;
-
     private int code;
 
-    public Item (String inName){
+    private int quantity;
+
+    private double price;
+
+    public Item (String inName, int inCode, int inQuantity, double inPrice){
+        this.name = inName;
+        this.code = inCode;
+        this.quantity = inQuantity;
+        this.price = inPrice;
 
     }
+
+    //Method to get all info about item excluding the code and return as string. To display on the kiosk.
+    public String allInfo()
+    {
+        String info = new String (name + " £" + price + " (" + quantity + " in stock)" );
+        return info;
+    }
+
+    public String basketInfo(){
+        String basketInfo = new String (name + " £" + price);
+        return basketInfo;
+    }
+
+    //Getters and setters
+    public String getName(){
+        return this.name;
+    }
+
+    public void setName(String inName){
+        this.name = inName;
+    }
+
+    public int getQuantity(){
+        return this.quantity;
+    }
+
+    public void setQuantity(int inQuantity){
+        this.quantity = inQuantity;
+    }
+
+    public int getCode(){
+        return this.code;
+    }
+
+    public double getPrice(){
+        return this.price;
+    }
+
+    public void setPrice(double inPrice){
+        this.price = inPrice;
+    }
+
+
+
+
+
 
 
 }
